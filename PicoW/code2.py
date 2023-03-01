6040fb5bea83c3eeaf08aafc34ef0e3e35a018a2
TheDarkGammers <98757458+TheDarkGammers@users.noreply.github.com>
2023-03-01 12:04:08 -0600

    test

--- PicoW/code2.py
+++ PicoW/code2.py
@@ -4,6 +4,7 @@ import digitalio
 from hx711_gpio import *
 from GETLogger import *
 from massSensor import *
+from gridAwe import *
 #this is the "LOGTOSEVER" code
 
 
@@ -33,22 +34,36 @@ logger = GETLogger("TFS Students", "Fultoneagles", "http://popu.local/WWT/webpag
 
 #calabrate the sensor using reading (x) and the mass (y) of the scale for two different masses.
 sensor = massSensor(652055,1510,911010,2084)
+old_mass=0
+#set up LEDs
+grid=gridAwe()
+grid.hourglass()
 
 while True:
     try:
         #scale equation
         x = hx.read()
-        real_mass = sensor.ymass(x)
+        real_mass = int(round (sensor.ymass(x),-1))
 
         # real_mass = sensor.getMB()*x+63.5
 #EACH SCALE HAST TO BE CALIBRATED I.E A 20Kg SCALE WILL HAVE A DIFFERENT EQUATION TO A 5Kg SCALE.
         if old_mass != real_mass:
+           #code below if for LEDs
+            grid.hourglass(True)
+            logger.log(data)
+            time.sleep(1)
+            grid.blankScreen()
+            grid.hourglass()
+            #sends data
             time.sleep(5)
             x = hx.read()
-            real_mass = sensor.ymass(x)
+            real_mass = int(round (sensor.ymass(x),-1))
             data["reading"] = real_mass
-            logger.log(data)
+            #grid.smileyFace()
+            
+             
         old_mass = real_mass
-        time.sleep(5)
+        time.sleep(1)
     except Exception as e:
         print("Error:\n", str(e))
+
